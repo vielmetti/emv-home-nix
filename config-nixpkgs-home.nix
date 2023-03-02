8e2002e079c96b9415ab496a0c2cfc58579a8194
Edward Vielmetti <edward.vielmetti@gmail.com>
2023-03-01 20:37:17 -0500

    initial config
    Signed-off-by: Edward Vielmetti <edward.vielmetti@gmail.com>

---
+++ config-nixpkgs-home.nix
@@ -0,0 +1,43 @@
+{ config, pkgs, ... }:
+
+{
+  home.username = "emv";
+  home.homeDirectory = "/home/emv";
+  home.stateVersion = "22.11";
+  programs.home-manager.enable = true;
+
+  home.packages = [
+    pkgs.htop
+    pkgs.fortune
+    pkgs.csvkit
+    pkgs.tailscale
+    pkgs.any-nix-shell
+    pkgs.atuin
+  ];
+
+  programs.git = {
+    enable = true;
+    userName  = "vielmetti";
+    userEmail = "edward.vielmetti@gmail.com";
+    extraConfig = {
+      credential.helper = "${
+          pkgs.git.override { withLibsecret = true; }
+        }/bin/git-credential-libsecret";
+    };
+  };
+
+  programs.zsh = {
+    enable = true;
+    history = {
+      size = 10000;
+      path = "${config.xdg.dataHome}/zsh/history";
+    };
+    zplug = {
+      enable = true;
+      plugins = [
+              { name = "romkatv/powerlevel10k"; tags = [ as:theme depth:1 ]; }
+              { name = "atuin"; }
+      ];
+    };
+  };
+}
